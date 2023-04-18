2f83d58c292034ba313b0f4ce9581e2193b2e48d
Joshua Yang <quickchicken02@gmail.com>
2023-04-18 02:08:08 -0500

    asdsdsad

--- attempts/attempt-5/final/src/App.js
+++ attempts/attempt-5/final/src/App.js
@@ -1,9 +1,24 @@
 import * as React from 'react';
 
+
+import TopBar from './components/TopBar';
+import { CssBaseline } from '@mui/material';
+
 function App() {
   return (
     <>
-    
+      {/* <CssBaseline/> */}
+
+      {/* 1.) Top header */}
+      <TopBar/>
+      
+      {/* 2.) Social header */}
+
+      {/* 3.) Scrollable content */}
+
+      {/* Bottom navigation section */}
+
+
     </>
   );
 }
