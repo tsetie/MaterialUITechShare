4c94766413ab6bd95d48dada0bcf084e12aaca15
Josh Yang <114260632+boovs@users.noreply.github.com>
2023-04-23 08:24:21 -0500

    set up for final template

---
+++ mui-template-finished/src/components/Socials.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import { Avatar, CssBaseline, Stack } from '@mui/material';
+import {styled} from '@mui/system';
+
+// Styled components method
+const StyledAvatar = styled(Avatar)( () => ({
+    width: '53px',
+    height: '53px',
+}));
+
+export default function Stories() {
+  return (
+    <>
+      <CssBaseline/>
+
+      {/* 2.) Social header */}
+      <Stack direction={'row'} spacing={2} sx={{display: 'flex', justifyContent: 'center', my: 2}}>
+        <Avatar/>
+        <Avatar/>
+        <Avatar/>
+        <Avatar/>
+        <Avatar/>
+      </Stack>
+
+    </>
+  );
+}
