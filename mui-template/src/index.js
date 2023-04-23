774212cf3a59c7e539e86a03017a0f440292f615
Josh Yang <114260632+boovs@users.noreply.github.com>
2023-04-23 00:58:57 -0500

    set up for mui template

--- mui-template/src/index.js
+++ mui-template/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material';
-
-import ourTheme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={ourTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
